2b373d3961b09c87f07f78cf679e33e7aaf47255
Rémy Lemeunier <remy.lemeunier@blablacar.com>
2016-06-16 20:04:18 +0200

    First try save img

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -7,6 +7,13 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Forms;
 
 class DefaultController extends Controller
 {
@@ -39,16 +46,46 @@ class DefaultController extends Controller
      */
     public function postAction(Request $request)
     {
-        $post = new Post();
-        $post->setText("description");
-        $post->setTripId(1);
-        $post->setUserId(2);
-        $post->setUsers(array(1,2,3));
-        $post->setImageUrl("/tmp/to/img.jpeg");
+        // var_dump($request->files->all());
+        // var_dump($request->request->all());exit;
+        $form = $this->createFormBuilder(null, array('csrf_protection' => false))
+            ->add('description', TextType::class)
+            ->add('trip_id',IntegerType::class)
+            ->add('user_id', IntegerType::class)
+            ->add('img', FileType::class)
+            // ->add('users', CollectionType::class, array('entry_type' => IntegerType::class))
+            ->getForm();
 
-        $em = $this->getDoctrine()->getManager();
-        $em->persist($post);
-        $em->flush();
+        $form->submit(array_merge($request->request->all(),$request->files->all()));
+        if ($form->isValid()) {
+            $data = $form->getData();
+
+            $file = $data['img'];
+            $fileName = md5(uniqid()).'.'.$file->guessExtension();
+            $file->move(
+                "/tmp",
+                $fileName
+            );
+
+            $post = new Post();
+            $post->setDescription($data['description']);
+            $post->setTripId($data['trip_id']);
+            $post->setUserId($data['user_id']);
+            $post->setUsers(array(1,2,3));
+            $post->setUrl($fileName);
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($post);
+            $em->flush();
+
+        }
+
+        foreach ($form->getErrors(true, false) as $error) {
+            echo 'la';
+            var_dump($error->getMessage());
+        }
+        echo 'la';exit;
+        exit;
 
         return array();
     }
